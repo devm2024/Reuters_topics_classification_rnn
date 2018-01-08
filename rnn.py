8f3b25d954011d7a1fcf65cbfc43f0896238c4d7
Devesh <dev.elect.iitd@gmail.com>
2018-01-08 06:24:49 -0800

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -25,15 +25,37 @@ y_train_en = to_categorical(y_train)
 y_test_en = to_categorical(y_test)
 
 # create the model
-embedding_vecor_length = 32
+embedding_vecor_length = 64
 model = Sequential()
 model.add(Embedding(top_words, embedding_vecor_length, input_length=250))
-model.add(LSTM(100))
+model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(LSTM(200))
+model.add(Dense(100))
 model.add(Dense(46))
 model.add(Activation('softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
-model.fit(x_train, y_train_en, validation_data=(x_test, y_test_en), epochs=5, batch_size=128)
+model.fit(x_train, y_train_en, validation_data=(x_test, y_test_en), epochs=5, batch_size=64)
 
 #Done, can be ran in notebook or here
+# Without CNN, output was 63% accuracy
+# With CNN, output is 
+# 65% accuracy in 46 classes is really good
+''' Here is the output of the model
+Train on 7602 samples, validate on 1901 samples
+Epoch 1/5
+7602/7602 [==============================] - 89s - loss: 2.0788 - acc: 0.4559 - val_loss: 1.7150 - val_acc: 0.5850
+Epoch 2/5
+7602/7602 [==============================] - 87s - loss: 1.5937 - acc: 0.5710 - val_loss: 1.6624 - val_acc: 0.5634
+Epoch 3/5
+7602/7602 [==============================] - 81s - loss: 1.4152 - acc: 0.6251 - val_loss: 1.4828 - val_acc: 0.6376
+Epoch 4/5
+7602/7602 [==============================] - 81s - loss: 1.2608 - acc: 0.6711 - val_loss: 1.4203 - val_acc: 0.6633
+Epoch 5/5
+7602/7602 [==============================] - 81s - loss: 1.1208 - acc: 0.7059 - val_loss: 1.4299 - val_acc: 0.6707
+'''
+
+
+
